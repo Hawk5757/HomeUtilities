9962b8928276c989f18a9069c51712790c340a39
Artem Herasymenko <artemka5757@gmail.com>
2022-08-17 23:53:19 +0300

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HomeUtilities.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
